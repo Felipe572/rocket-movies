f735b1fdf966c378ee4dc6fa12210f8432512055
Felipe <felipemachado572@gmail.com>
2025-01-15 10:41:45 -0300

    Iniciando tela principal

--- rocketmovies/src/main.jsx
+++ rocketmovies/src/main.jsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import Details from './pages/Details/Details';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Details />
   </StrictMode>,
 )
---
+++ rocketmovies/src/pages/Details/Details.jsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles";
+
+export default function Details(){
+  return (
+    <Container>
+      <h1>Hello world</h1>
+    </Container>
+  );
+}
