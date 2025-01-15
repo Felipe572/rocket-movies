74b3535d5362326708d8fb2e0715d05fd868c134
Felipe <felipemachado572@gmail.com>
2025-01-15 11:34:47 -0300

    Add style global

--- rocketmovies/src/main.jsx
+++ rocketmovies/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import GlobalStyle from './styles/global';
+
+import theme from './styles/theme';
+import { ThemeProvider } from 'styled-components';
 
 import Details from './pages/Details/Details';
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Details />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Details />
+    </ThemeProvider>
   </StrictMode>,
 )
--- rocketmovies/src/pages/Details/Details.jsx
+++ rocketmovies/src/pages/Details/Details.jsx
@@ -4,6 +4,8 @@ export default function Details(){
   return (
     <Container>
       <h1>Hello world</h1>
+
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis sequi doloribus reiciendis nisi atque placeat vero eaque inventore aliquid officiis iste numquam cumque, at aut eum blanditiis provident totam culpa?</p>
     </Container>
   );
 }
