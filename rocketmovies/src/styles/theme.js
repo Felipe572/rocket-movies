9aa712bf740d715770ab9be7c537be3c80d1bbb5
Felipe <felipemachado572@gmail.com>
2025-01-15 11:18:31 -0300

    Create theme

---
+++ rocketmovies/src/styles/theme.js
@@ -0,0 +1,16 @@
+export default {
+    COLORS: {
+        BACKGROUND_900: "#1c1b1e",
+        BACKGROUND_800: "#312E38",
+        BACKGROUND_700: "#3E3B47",
+
+        WHITE: "#F4EDE8",
+        PINK: "#FF859B",
+
+        GRAY_100: "#948f99",
+        GRAY_200: "#999591",
+        GRAY_300: "#262529"
+
+
+    }
+}
